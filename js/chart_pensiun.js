82b224ce32ccf1d178dd6c4e8d62fa3c0aff08e9
miscla <feisalfarhanudin@gmail.com>
2017-12-15 18:08:13 +0700

    all

--- js/chart_pensiun.js
+++ js/chart_pensiun.js
@@ -1,32 +1,16 @@
 var ctx = document.getElementById('myChart').getContext('2d');
 var chart = new Chart(ctx, {
     // The type of chart we want to create
-    type: 'line',
+    type: 'horizontalBar',
 
     // The data for our dataset
     data: {
-        labels: ["Januari", "Februari", "Maret", "April", "Mei", "Juni", "Juli", "Juli", "Agustus", "September", "Oktober", "November", "Desember"],
+        labels: ["1 - 6 Bulan", "6 - 12 Bulan", "12 Bulan ++"],
         datasets: [{
-            label: "Pensiunan yang Berhak",
-            borderColor: 'rgb(255, 99, 132)',
-            data: [154, 100, 200, 193, 354, 120, 290, 140, 222, 300, 230, 30, 0],
-            fill: false,
-            borderWidth: 1,
-        }, {
-            label: "Nominal Uang",
-            borderColor: 'rgb(0, 0, 255)',
-            data: [176, 90, 230, 180, 354, 140, 300, 123, 223, 240, 200, 360, 320],
-            fill: false,
-            borderWidth: 1,
+            label: "Pensiunan yang Ditangguhkan",
+            borderColor: ['rgb(0, 255, 0)', 'rgb(255, 255, 0)', 'rgb(255, 0, 0)'],
+            backgroundColor: ['rgb(0, 255, 0)', 'rgb(255, 255, 0)', 'rgb(255, 0, 0)'],
+            data: [116, 28, 46],
         }]
     },
-
-    // Configuration options go here
-    options: {
-        elements: {
-            line: {
-                tension: 0, // disables bezier curves
-            }
-        }
-    }
 });
